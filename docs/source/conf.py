610625d7db256919b43c4faf1b72b32cff681749
Geoff Klein <geoff.a.klein@gmail.com>
2023-07-20 14:08:57 -0400

    trying something else

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,4 +43,4 @@ intersphinx_mapping = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
