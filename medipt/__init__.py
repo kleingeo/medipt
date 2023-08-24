b56464950351e80d29fe545ee57d87fb72077607
Geoff Klein <geoff.a.klein@gmail.com>
2023-08-23 22:25:42 -0400

    renamed main directory to lower case
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda working
    
    trying to get conda
    
    stuff
    
    stuff
    
    stuff
    
    I think it works?
    
    Conda and pypi working
    
    Conda and pypi working in single action. Removing old actions
    
    version not transferring to conda meta.yaml
    
    version not transferring to conda meta.yaml
    
    version for automating conda working. Testing with single build and publish. Removed auto-building for documentation until working better. Also removed also dist and wheel building for publishing package
    
    numpy version for python 3.7 was too high
    
    added channels to call
    
    fixing
    
    fixing
    
    Working i think
    
    fixing issue where could not install packages on server
    
    Issue with remembering which channels
    
    conda channel issue
    
    fixing
    
    removing additional channels
    
    i dont know why this doesnt work
    
    maybe its working?
    
    sweet jesus i think it works
    
    Finally working - auto version not working
    
    Package created and can be installed now. Auto versioning still not working with pyproject.toml
    Changed `buildplat` to `os`
    
    still failed, python version not fixed and changed during conda build
    
    failed with miniconda to get dependencies
    
    trying with micromamb
    
    fixing master to main
    
    something
    
    something
    
    something
    
    something
    
    something
    
    something
    
    something
    
    something
    
    something
    
    something
    
    something
    
    versioneer working
    
    test changes
    
    issue with versioneer and conda build, trying to fix

--- medipt/__init__.py
+++ medipt/__init__.py
@@ -2,5 +2,13 @@
 # from . import transforms
 # from . import resample_image
 
+import transforms
+import utils
+import resample_image
 
-# __all__ = ['utils',]
+# __all__ = ['utils',
+#            'transforms',
+#            'resample_image',
+#            ]
+from . import _version
+__version__ = _version.get_versions()['version']
