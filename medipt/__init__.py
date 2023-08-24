907cf9b766be8077da858dd35c0af421fa01d1cc
Geoff Klein <geoff.a.klein@gmail.com>
2023-08-24 00:24:12 -0400

    Removing versioneer, trying setuptools_scm

--- medipt/__init__.py
+++ medipt/__init__.py
@@ -2,13 +2,11 @@
 # from . import transforms
 # from . import resample_image
 
-import transforms
-import utils
-import resample_image
+import medipt.transforms
+import medipt.utils
+import medipt.resample_image
 
 # __all__ = ['utils',
 #            'transforms',
 #            'resample_image',
 #            ]
-from . import _version
-__version__ = _version.get_versions()['version']
