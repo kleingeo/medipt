a17cf12ceeb657d5d7df9e5dab6671c928f5f360
Geoff Klein <geoff.a.klein@gmail.com>
2023-08-25 19:04:16 -0400

    add set_num_threads

--- medipt/utils/__init__.py
+++ medipt/utils/__init__.py
@@ -1,2 +1,7 @@
 from .random_float import random_uniform_float, random_binomial
 from .spatial_utils import *
+import SimpleITK as sitk
+
+
+def set_num_workers(n_threads):
+    sitk.ProcessObject.SetGlobalDefaultNumberOfThreads(n_threads)
